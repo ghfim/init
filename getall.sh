e2b6d48cd2a988133a26ac20b7e5b40de80b3563
eoe2005 <eoe2005@hotmail.com>
2025-03-24 21:02:08 +0800

    Create getall.sh

---
+++ getall.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd ..
+git config --global user.name "吃掉大象的蚂蚁"
+git config --global user.email 'eoe2005@gmail.com'
+
+git clone git@github.com:ghfim/im-flutter.git
+
+git clone git@github.com:ghfim/server.git
